4d5bfef4fbcc6a7740f593d052abdc7b2273cc5f
Joel Rocha <joelxr@gmail.com>
2015-06-14 10:56:03 -0300

    Alteração na classe de entropia e HuffMan para utilizar TreeSet

---
+++ src/main/java/br/edu/ifce/calculation/Huffman.java
@@ -0,0 +1,110 @@
+package br.edu.ifce.calculation;
+
+import br.edu.ifce.data.Node;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
+
+/**
+ * @author Joel Rocha
+ * @author Yuri Oliveira
+ */
+public class Huffman {
+
+    private TreeSet<Node> dictionary;
+
+    public Huffman() {
+        dictionary = new TreeSet<Node>();
+    }
+
+    public static void main(String[] args) {
+        Huffman huffman = new Huffman();
+        huffman.readData();
+        System.out.println("Occurrences: " + huffman.getDictionary());
+        System.out.println("Entropy: " + huffman.calculateEntropy());
+    }
+
+    public void readData() {
+        List<String> data = null;
+        String content = "";
+
+        try {
+            data = readFile("data.txt");
+
+            for (String s : data) {
+                content += s;
+            }
+
+            for (char c : content.toCharArray()) {
+                Node node = new Node(c);
+
+                if (dictionary.contains(node)) {
+                    Node aux = dictionary.headSet(node, true).last();
+                    int count = aux.getFrequency();
+                    aux.setFrequency(++count);
+                } else {
+                    dictionary.add(node);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public List<String> readFile(String filename) throws IOException {
+        List<String> records = new ArrayList<String>();
+        BufferedReader reader = null;
+        String line;
+
+        try {
+            reader = new BufferedReader(new FileReader(filename));
+
+            while ((line = reader.readLine()) != null) {
+                records.add(line);
+            }
+        } finally {
+            if (reader != null)
+                reader.close();
+        }
+
+        return records;
+    }
+
+    public double calculateEntropy() {
+        double totalFrequency = getTotalFrequency();
+        double entropy = 0.0;
+
+        for (Node node : dictionary) {
+            int frequency = node.getFrequency();
+            double probability = frequency / totalFrequency;
+            node.setProbability(probability);
+            entropy += probability * Math.log(probability) / Math.log(2);
+        }
+
+        entropy = -entropy;
+        return entropy;
+    }
+
+    private double getTotalFrequency() {
+        double totalTimes = 0;
+
+        for (Node node : dictionary) {
+            totalTimes += node.getFrequency();
+        }
+
+        return totalTimes;
+    }
+
+    public TreeSet<Node> getDictionary() {
+        return dictionary;
+    }
+
+    public void setDictionary(TreeSet<Node> dictionary) {
+        this.dictionary = dictionary;
+    }
+}
---
+++ src/main/java/br/edu/ifce/data/Node.java
@@ -0,0 +1,131 @@
+package br.edu.ifce.data;
+
+public class Node implements Comparable<Node> {
+
+    private char value;
+    private String trace;
+    private int frequency;
+    private double probability;
+    private Node left;
+    private Node right;
+    private Node root;
+    private int key;
+
+    public Node(char data) {
+        this.value = data;
+        this.trace = "" + data;
+        this.frequency = 1;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    public char getValue() {
+        return value;
+    }
+
+    public void setValue(char value) {
+        this.value = value;
+    }
+
+    public String getTrace() {
+        return trace;
+    }
+
+    public void setTrace(String trace) {
+        this.trace = trace;
+    }
+
+    public double getProbability() {
+        return probability;
+    }
+
+    public void setProbability(double probability) {
+        this.probability = probability;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public int compareTo(Node node) {
+        int result;
+
+        result = compareDouble(this.getProbability(), node.getProbability());
+        if (result != 0) return result;
+
+        result = compareInt(this.getValue(), node.getValue());
+        return result;
+    }
+
+    private static int compareInt(int i1, int i2) {
+        if (i1 < i2) {
+            return -1;
+        } else if (i1 > i2) {
+            return +1;
+        } else {
+            return 0;
+        }
+    }
+
+    private static int compareDouble(double d1, double d2) {
+        if (d1 < d2) {
+            return -1;
+        } else if (d1 > d2) {
+            return +1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return this.compareTo(node) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) getValue();
+    }
+
+    @Override
+    public String toString() {
+        return value + "{trace=" + trace + ",frequency=" + frequency + ",probability=" + probability + "}";
+    }
+}
